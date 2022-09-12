6495074f342553e85fb02f4fb3a7207e4acd395e
Minh Tam <tamnm1996@gmail.com>
2022-09-12 20:25:56 +0700

    [MARK] - Outlets

--- GIT-TEST-Flow/GIT-TEST-Flow/Module/LogIn/View/ViewController.swift
+++ GIT-TEST-Flow/GIT-TEST-Flow/Module/LogIn/View/ViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //MARK: - Properties
-
+    //MARK: Outlets
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
