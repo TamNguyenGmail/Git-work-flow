4e1e59278afb4fc5136b557b51b3aec46603fdba
Tom Nguyen <minhtamnguyen@motorist.sgminhtamnguyen@motorist.sg>
2022-09-13 10:17:20 +0700

    [ADD] map module

---
+++ GIT-TEST-Flow/GIT-TEST-Flow/Module/Map/View/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  GIT-TEST-Flow
+//
+//  Created by motorist on 13/09/2022.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    //MARK: thuan
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
