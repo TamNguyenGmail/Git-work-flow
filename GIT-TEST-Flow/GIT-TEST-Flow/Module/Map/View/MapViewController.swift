b8c823014f76709d20cf9a5373e69f5aa1b1d621
Tom Nguyen <minhtamnguyen@motorist.sgminhtamnguyen@motorist.sg>
2022-09-13 16:51:05 +0700

    [UPDATE] files

---
+++ GIT-TEST-Flow/GIT-TEST-Flow/Module/Map/View/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewController.swift
+//  GIT-TEST-Flow
+//
+//  Created by motorist on 13/09/2022.
+//
+
+import UIKit
+
+class MapViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
