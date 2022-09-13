a508767533b5d8fe96c5493378bc23dd9ea32b91
Tom Nguyen <minhtamnguyen@motorist.sgminhtamnguyen@motorist.sg>
2022-09-13 14:54:43 +0700

    [UI] cherry pick soft-reset to pick-table-view

--- GIT-TEST-Flow/GIT-TEST-Flow/Module/LogIn/View/ViewController.swift
+++ GIT-TEST-Flow/GIT-TEST-Flow/Module/LogIn/View/ViewController.swift
@@ -8,13 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //MARK: Trinh
+    //MARK: Properties
+    private lazy var userTableView: UITableView = {
+        let table = UITableView()
+        table.dataSource = self
+        table.register(UserCell.self, forCellReuseIdentifier: "UserCell")
+        return table
+    }()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //MARK: Life cycle
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        view.addSubview(userTableView)
+        userTableView.frame = view.bounds
+        
     }
 
+}
 
+//MARK: - Data source
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as? UserCell else {return UITableViewCell()}
+        
+        cell.setupBackgroundColor(color: .cyan)
+        
+        return cell
+        
+    }
+    
 }
 
