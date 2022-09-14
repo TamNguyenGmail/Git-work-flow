c504021e7eb19ec51218f5bd3ffb8c2e89b2f6c9
Tom Nguyen <minhtamnguyen@motorist.sgminhtamnguyen@motorist.sg>
2022-09-14 10:28:09 +0700

    [UI] add table view in VR module

--- GIT-TEST-Flow/GIT-TEST-Flow/Module/VR/View/VRViewController.swift
+++ GIT-TEST-Flow/GIT-TEST-Flow/Module/VR/View/VRViewController.swift
@@ -8,22 +8,38 @@
 import UIKit
 
 class VRViewController: UIViewController {
+    //MARK: - Properties
+    private lazy var vrTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.register(UserCell.self, forCellReuseIdentifier: "UserCell")
+        return tableView
+    }()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    //MARK: - Life cycle
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        view.addSubview(vrTableView)
+        vrTableView.frame = view.bounds
+        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+//MARK: - Data source
+extension VRViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as? UserCell else {return UITableViewCell()}
+        
+        return cell
+        
+    }
+    
+}
+
